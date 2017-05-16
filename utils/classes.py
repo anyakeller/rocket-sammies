8648cb2593525cd96dcac64a0a0af231c3b9348e
James Wang <jameswang9909@hotmail.com>
2017-05-16 15:29:50 -0400

    Add functions for classes

---
+++ utils/classes.py
@@ -0,0 +1,51 @@
+import common
+
+def create_class(name, tid, students=[]):
+    """
+    Creates a class and inserts it into the database
+    Returns the cid of the new class
+    """
+    db = common.get_connection()
+    cid = common.random_string()
+    class_ = {
+        "cid": cid,
+        "name": name,
+        "students": students,
+        "teacher": tid,
+        "assignments": [],
+        "weightmap": {},
+    }
+
+    db.classes.insert(class_)
+    return cid
+
+def get_class(cid):
+    """
+    Retrieve a class from its class id (cid)
+    Returns None if the class does not exist
+    """
+
+    db = common.get_connection()
+    match = {
+        "cid": cid
+    }
+    class_ = db.classes.find_one(match)
+
+    return class_
+
+def get_classes():
+    """Retrieve a list of all classes"""
+
+    db = common.get_connection()
+    classes = db.classes.find()
+
+    return list(classes)
+
+def remove_class(cid):
+    """Delete a class from its class id"""
+
+    db = common.get_connection()
+    match = {
+        "cid": cid
+    }
+    db.classes.delete_one(match)
