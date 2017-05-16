9b29478adef27598207b6b4f627bdcd33ab7f4f7
James Wang <jameswang9909@hotmail.com>
2017-05-16 13:54:05 -0400

    Add function for generating a random string

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 import hashlib
+import random
 
 DATABASE_NAME = "project_manager"
 HOST = "127.0.0.1"
@@ -25,3 +26,8 @@ def get_connection():
 def hash(s):
     """Hash the given string"""
     return hashlib.sha256(s).hexdigest()
+
+def random_string(length=32):
+    """Generate a random string of a specific length (default is 32)"""
+    alphabet = "abcdef0123456789"
+    return "".join(random.choice(alphabet) for x in range(length))
