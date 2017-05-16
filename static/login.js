eb64bc0b99b3cb62ced14050771df6424b13679c
Wilson Berkow <wilsonberkow@gmail.com>
2017-05-16 14:01:50 -0400

    Implement Ajax calls for login and register

--- static/login.js
+++ static/login.js
@@ -1,20 +1,72 @@
 (function () {
     "use strict";
 
-    var apiCall = function (method, url, data, success) {
+    var apiCall = function (method, url, data, success, failure) {
 	$.ajax({
 	    method: method,
 	    url: url,
-	    data: data,
+	    data: JSON.stringify(data),
 	    dataType: 'json'
 	}).then(function (data, status, jqxhr) {
-	    if (typeof data.redirect == "string") {
-		window.location = data.redirect;
-	    } else {
+	    if (typeof success === "function") {
 		success(data, status, jqxhr);
 	    }
-	}, function (jqxhr, status, err) {
-	    console.err("XHR returned error: " + err);
+	    if (typeof data.redirect === "string") {
+		window.location = data.redirect;
+	    }
+	}, failure);
+    };
+
+    var apiLogin = function (email, pass) {
+	apiCall("POST", "/api/user/login", {
+	    email: email,
+	    password: pass
+	}, function (response) {
+	    if (response.success === 1) {
+		window.location = "/dashboard";
+	    } else {
+		// TODO: better popup
+		// TODO: better message (once backend is more detailed)
+		alert("Authentication failed");
+	    }
 	});
     };
+
+    var apiRegister = function (email, pass) {
+	apiCall("POST", "/api/user/register", {
+	    email: email,
+	    password: pass
+	}, function (response) {
+	    if (response.success === 1) {
+		window.location = "/dashboard";
+	    } else {
+		// TODO: better popup
+		// TODO: better message (once backend is more detailed)
+		alert("Authentication failed");
+	    }
+	});
+    };
+
+    var loginBtn = document.getElementById("btn-login");
+    var loginUName = document.getElementById("login-uname");
+    var loginPass = document.getElementById("login-pass");
+    var registerBtn = document.getElementById("btn-register");
+    var registerUName = document.getElementById("register-uname");
+    var registerPass1 = document.getElementById("register-pass1");
+    var registerPass2 = document.getElementById("register-pass2");
+
+    loginBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+	apiLogin(loginUName.value, loginPass.value);
+    });
+
+    registerBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+	if (registerPass1.value !== registerPass2.value) {
+	    // TODO: better alerts
+	    alert("Passwords do not match!");
+	} else {
+	    apiRegister(registerUName.value, registerPass1.value);
+	}
+    });
 }());
