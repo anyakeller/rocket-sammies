f64c793344ecc75818f3b12bb593635e2bf437fb
James Wang <jameswang9909@hotmail.com>
2017-05-16 23:37:59 -0400

    Implement route for creating assignments

--- api/assignment.py
+++ api/assignment.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
-from decorators import api_wrapper, login_required, teachers_only
+from decorators import WebException, api_wrapper, login_required, teachers_only
+from utils import assignments
 
 blueprint = Blueprint("assignment", __name__)
 
@@ -10,7 +11,36 @@ blueprint = Blueprint("assignment", __name__)
 @login_required
 def create_assignment():
     """ Route for creating an assignment """
-    # TODO: Implement
+
+    form = request.form
+    cid = form.get("cid")
+    title = form.get("title")
+    description = form.get("description")
+    max_score = form.get("max_score")
+    _type = form.get("type")
+
+    try:
+        # Validate max_score
+        max_score = int(max_score)
+        assert max_score > 0
+    except:
+        raise WebException("Max score must be greater than 0")
+
+    if _type == assignments.PROJECT:
+        # Create a project assignment
+        rubric = {} # TODO: Implement
+
+        try:
+            # Validate max_group_size
+            max_group_size = int(form.get("max_group_size"))
+            assert max_group_size > 1
+        except:
+            raise WebException("Maximum group size must be at least 2")
+
+        assignments.create_project(cid, title, description, max_score, max_group_size, rubric)
+    else:
+        assignments.create_assignment(cid, title, description, max_score, _type)
+
     return { "success": 1, "message": "Assignment created" }
 
 @blueprint.route("/submit", methods=["POST"])
