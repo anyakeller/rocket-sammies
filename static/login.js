59a263f18ee6e79204e25634d3463b53968da2d7
James Wang <jameswang9909@hotmail.com>
2017-05-21 08:04:57 -0400

    Use forms for login and registration

--- static/login.js
+++ static/login.js
@@ -1,38 +1,39 @@
 (function () {
     "use strict";
     var apiLogin = function (email, pass) {
-	PM.apiCall("POST", "/api/user/login", {
-	    email: email,
-	    password: pass
-	}, "/dashboard");
+        PM.apiCall("POST", "/api/user/login", {
+            email: email,
+            password: pass
+        }, "/dashboard");
     };
 
     var apiRegister = function (email, pass) {
-	PM.apiCall("POST", "/api/user/register", {
-	    email: email,
-	    password: pass
-	}, "/dashboard");
+        PM.apiCall("POST", "/api/user/register", {
+            email: email,
+            password: pass
+        }, "/dashboard");
     };
 
-    var loginBtn = document.getElementById("btn-login");
+    var loginForm = document.getElementById("login-form");
     var loginUName = document.getElementById("login-uname");
     var loginPass = document.getElementById("login-pass");
-    var registerBtn = document.getElementById("btn-register");
+
+    var registerForm = document.getElementById("register-form");
     var registerUName = document.getElementById("register-uname");
     var registerPass1 = document.getElementById("register-pass1");
     var registerPass2 = document.getElementById("register-pass2");
 
-    loginBtn.addEventListener("click", function (e) {
-	e.preventDefault();
-	apiLogin(loginUName.value, loginPass.value);
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        apiLogin(loginUName.value, loginPass.value);
     });
 
-    registerBtn.addEventListener("click", function (e) {
-	e.preventDefault();
-	if (registerPass1.value !== registerPass2.value) {
-	    $.notify("Passwords do not match!", "error");
-	} else {
-	    apiRegister(registerUName.value, registerPass1.value);
-	}
+    registerForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        if (registerPass1.value !== registerPass2.value) {
+            $.notify("Passwords do not match!", "error");
+        } else {
+            apiRegister(registerUName.value, registerPass1.value);
+        }
     });
 }());
