68c72875a9dab366bb0356a2d9c8f45c48301858
Wilson Berkow <wilsonberkow@gmail.com>
2017-05-21 09:25:21 +0800

    Allow frontend to initialize classes in database

--- static/class.js
+++ static/class.js
@@ -1,12 +1,19 @@
 (function () {
     "use strict";
     var btnNewClass = document.getElementById("btn-new-class");
-    var newClassModal = document.getElementById("modal-new-class");
+    var modalNewClass = document.getElementById("modal-new-class");
     var inputNewClassName = document.getElementById("input-new-class-name");
     var btnImportClass = document.getElementById("btn-import-class");
     var inputCreateClassStudents = document.getElementById("input-create-class-students");
+    var btnCreateClass = document.getElementById("btn-create-class");
 
     btnNewClass.addEventListener("click", function () {
-        $(newClassModal).modal();
+        $(modalNewClass).modal();
+    });
+
+    btnCreateClass.addEventListener("click", function () {
+        PM.apiCall("POST", "/api/class/create", {
+            "name": inputNewClassName.value
+        }, "/class");
     });
 }());
