41a63f978eff7cab3723d500bfb407c8a2d0dd06
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-05-24 22:49:19 -0400

    adding modal for assigning projects to students

--- static/class.js
+++ static/class.js
@@ -1,4 +1,4 @@
-(function () {
+  (function () {
     "use strict";
     var btnNewClass = document.getElementById("btn-new-class");
     var modalNewClass = document.getElementById("modal-new-class");
