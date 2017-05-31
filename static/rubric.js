5c54dde6e58e9c69a128f90842992980fd97bdf3
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-05-31 15:35:27 -0400

    added event listener to submit button for rubrics

--- static/rubric.js
+++ static/rubric.js
@@ -1,17 +1,28 @@
 (function () {
     "use strict";
-
     var categoryCreate = document.getElementById('create-rubric-row');
-    console.log(categoryCreate.value);
-    categoryCreate.addEventListener("submit", function (e) {
-      var categoryDescription = document.getElementById('user-rubric-category').value;
-      var categoryGrade = document.getElementById('user-rubric-grade').value;
-      console.log(categoryDescription);
-      console.log(categoryGrade);
+    var createRubricTable = function(e){
+      var categoryGrade = document.getElementById('user-rubric-grade');
+      var categoryDescription = document.getElementById('user-rubric-category');
+      // Create Table with the things
+      var table = document.createElement("table");
+      table.setAttribute("class","table table-inverse");
+      table.setAttribute("style","width:50%;");
+
+      var tablehead = document.createElement("thead");
+      var tr = document.createElement("tr");
+      var th = document.createElement("th");
+      var th2 = document.createElement('th');
+
+      th.setAttribute("style","width:85%;");
+      th2.setAttribute("style","width:15%;");
+      table.appendChild(tablehead);
+      table.appendChild(tr);
+      table.appendChild(th);
+      table.appendChild(th2);
+      document.appendChild(table);
 
 
-    });
-
-
-
+    }
+    categoryCreate.addEventListener("click",createRubricTable);
 }());
