3f5ca35eead07b61d4b299ab013bf741aaa4abeb
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-06-01 13:07:53 -0400

    yayy i finished rubric creation

--- static/rubric.js
+++ static/rubric.js
@@ -5,15 +5,15 @@
     var tableRef = initTable.getElementsByTagName('tbody')[0];
 
     var createRubricTable = function(e){
-      var categoryGrade = document.getElementById('user-rubric-grade');
-      var categoryDescription = document.getElementById('user-rubric-category');
+      var categoryGrade = document.getElementById('user-rubric-grade').value;
+      var categoryDescription = document.getElementById('user-rubric-category').value;
       // Create Table with the things
 
       var newRow = tableRef.insertRow(tableRef.rows.length);
       var newCell = newRow.insertCell(0);
       var newCell2 = newRow.insertCell(-1);
-      var newText = document.createTextNode("testingbutsbutsbuts");
-      var newText2 = document.createTextNode("BUTTSBUTTSBUTTSBUTTS");
+      var newText = document.createTextNode(categoryDescription);
+      var newText2 = document.createTextNode(categoryGrade);
       newCell.appendChild(newText);
       newCell2.appendChild(newText2);
       
