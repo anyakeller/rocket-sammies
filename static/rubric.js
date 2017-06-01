38ac40667cf67511e4f294b1d25a63082f2f4c29
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-06-01 13:29:44 -0400

    adding more rubric javscript stuff

--- static/rubric.js
+++ static/rubric.js
@@ -1,26 +1,23 @@
 (function () {
     "use strict";
     var categoryCreate = document.getElementById('create-rubric-row');
+    var initTable = document.getElementById('initial-table');
+    var tableRef = initTable.getElementsByTagName('tbody')[0];
+
     var createRubricTable = function(e){
       var categoryGrade = document.getElementById('user-rubric-grade');
       var categoryDescription = document.getElementById('user-rubric-category');
       // Create Table with the things
-      var table = document.createElement("table");
-      table.setAttribute("class","table table-inverse");
-      table.setAttribute("style","width:50%;");
 
-      var tablehead = document.createElement("thead");
-      var tr = document.createElement("tr");
-      var th = document.createElement("th");
-      var th2 = document.createElement('th');
+      var newRow = tableRef.insertRow(tableRef.rows.length);
+      var newCell = newRow.insertCell(0);
+      var newCell2 = newRow.insertCell(-1);
+      var newText = document.createTextNode("testingbutsbutsbuts");
+      var newText2 = document.createTextNode("BUTTSBUTTSBUTTSBUTTS");
+      newCell.appendChild(newText);
+      newCell2.appendChild(newText2);
+      
 
-      th.setAttribute("style","width:85%;");
-      th2.setAttribute("style","width:15%;");
-      table.appendChild(tablehead);
-      table.appendChild(tr);
-      table.appendChild(th);
-      table.appendChild(th2);
-      document.appendChild(table);
 
 
     }
