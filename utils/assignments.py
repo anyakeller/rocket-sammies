32cfba6236b5de8832c041ac1dd30612d0c63433
James Wang <jameswang9909@hotmail.com>
2017-06-03 13:00:42 -0400

    Change 'active' field to 'assigned'

--- utils/assignments.py
+++ utils/assignments.py
@@ -16,7 +16,7 @@ def create_assignment(cid, title, description, max_score, _type, rubric):
         "max_score": max_score,
         "rubric": rubric,
         "type": _type,
-        "active": False,
+        "assigned": False,
     }
 
     db.assignments.insert(assignment)
