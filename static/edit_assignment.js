d03a62c2492220681c36f26de625ae0c03460328
dhiraj-patel <dhirajsuperhero@gmail.com>
2017-06-04 13:44:35 -0400

    adding rubric javascript to the edit assignment page

--- static/edit_assignment.js
+++ static/edit_assignment.js
@@ -21,4 +21,62 @@
         }
         updateAssignment(aid, data);
     });
+
+
+      // THIS IS THE JAVASCRIPT FOR THE RUBRIC FORMS ADDED BY DHIRAJATORY STATEMENTS
+      var rubricRowCreate = document.getElementById('submit-to-rubric-table');
+      var openRubricModal = document.getElementById('create-rubric-row');
+      var modalForRubrics = document.getElementById("modal-edit-rubric");
+
+      var initTable = document.getElementById('initial-table');
+      var tableRef = initTable.getElementsByTagName('tbody')[0];
+
+      openRubricModal.addEventListener("click", function () {
+          $(modalForRubrics).modal();
+      });
+
+
+      var createRubricTable = function(e){
+          var categoryGrade = document.getElementById('user-rubric-grade').value;
+          var categoryDescription = document.getElementById('user-rubric-category').value;
+
+          // Create Table with the things
+          var newRow = tableRef.insertRow(tableRef.rows.length);
+          var newCell = newRow.insertCell(0);
+          var newCell2 = newRow.insertCell(-1);
+          var newText = document.createTextNode(categoryDescription);
+          var newText2 = document.createTextNode(categoryGrade);
+          newCell.appendChild(newText);
+          newCell2.appendChild(newText2);
+
+          // clear the modal input fields
+          document.getElementById("user-rubric-grade").value = "";
+          document.getElementById("user-rubric-category").value = "";
+
+
+          // send the data to backend using hidden html forms
+          $(modalForRubrics).modal('hide');
+
+      }
+      rubricRowCreate.addEventListener("click",createRubricTable);
+
+      var createRubricForm = function(e){
+          var form = document.createElement('form');
+          form.setAttribute('action',"/sendRubricData")
+          form.setAttribute('method','GET');
+          var hiddenInput = document.createElement('input');
+          hiddenInput.setAttribute('type','hidden');
+          form.appendChild(hiddenInput);
+
+      }
+
+
+
+
+
+
+
+
+
+
 }());
