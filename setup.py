d27c445b9064c0ca7f4e55fd4c91d6bab0c95581
James Wang <jameswang9909@hotmail.com>
2017-06-05 14:01:59 -0400

    Change setup script to only drop collections

--- setup.py
+++ setup.py
@@ -6,8 +6,3 @@ collections = ["assignments", "classes", "grades", "students", "users"]
 
 for collection in collections:
     db.drop_collection(collection)
-
-for collection in collections:
-    db.drop_collection(collection)
-
-utils.students.addStudents("data/sample.csv")
